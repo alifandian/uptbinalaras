0bd3cd30b0abd58949bd593859434df95f64fa85
alifandian <alifandianachmad@student.uir.ac.id>
2026-08-04 14:18:23 +0700

    add addpatient template to add patient data

--- patients/urls.py
+++ patients/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "patients"
 urlpatterns = [
     path("", views.list_patient, name="list_patient"),
     path("addpatient/", views.add_patient, name="add_patient"),
+    path("addpatient/add", views.add, name="add"),
     path("<int:patient_id>/", views.detail_patient, name="detail_patient"),
 ]
--- patients/views.py
+++ patients/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 from .models import Patient
 
 # Create your views here.
@@ -15,9 +16,33 @@ def add_patient(request):
 
 #detail setiap pasien
 def detail_patient(request, patient_id):
-    patientID = Patient.objects.get(pk=patient_id)
+    patientID = get_object_or_404(Patient, pk=patient_id)
     context = {"patient" : patientID}
     return render(request, "patients/detail_patient.html", context)
+
+def add(request):
+    try:
+        full_name=request.POST["full_name"]
+        gender=request.POST["gender"]
+        birth_date=request.POST["birth_date"]
+        admission_date=request.POST["admission_date"]
+        status=request.POST["status"]
+        address=request.POST["address"]
+
+    except KeyError:
+        return render(request, 
+                      "patients/add_patient.html",
+                      {"error_message" : "anda tidak mengisi form dengan lengkap "},)
+    
+    else :
+        p = Patient(full_name=full_name, 
+                    gender=gender, 
+                    birth_date=birth_date, 
+                    admission_date=admission_date,
+                    status=status,
+                    address=address,)
+        p.save()
+        return HttpResponseRedirect(reverse("patients:list_patient"))
 
 
 
